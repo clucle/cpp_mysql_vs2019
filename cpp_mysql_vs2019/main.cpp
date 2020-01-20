5c2b119160b91cc877dd159c36b77c53558a460a
clucle <wjdenwls123@gmail.com>
2020-01-20 20:52:13 +0900

    feat: connect database
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- cpp_mysql_vs2019/main.cpp
+++ cpp_mysql_vs2019/main.cpp
@@ -1,7 +1,26 @@
-
+﻿
+#include <stdlib.h>
 #include <iostream>
 
-int main()
+#include <jdbc/cppconn/driver.h>
+#include <jdbc/cppconn/exception.h>
+#include <jdbc/cppconn/resultset.h>
+#include <jdbc/cppconn/statement.h>
+
+using namespace std;
+
+int main(void)
 {
-	std::cout << "Hello World!\n";
+	sql::Driver* driver;
+	sql::Connection* con;
+
+	driver = get_driver_instance();
+	con = driver->connect("tcp://127.0.0.1:3306", "admin", "w2aredeveloper");
+	con->setSchema("sampledatabase");
+
+	delete con;
+
+	cout << endl;
+
+	return EXIT_SUCCESS;
 }
