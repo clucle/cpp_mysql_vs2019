f18de0dfe919c32a9280438da92ed37cd7c29469
clucle <wjdenwls123@gmail.com>
2020-01-24 21:45:00 +0900

    feat: make database class non-copyable
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- cpp_mysql_vs2019/database.h
+++ cpp_mysql_vs2019/database.h
@@ -15,6 +15,10 @@ class Database
 public:
 	Database(std::string url, std::string user, std::string password);
 	~Database();
+
+	Database(const Database&) = delete;
+	Database& operator=(const Database&) = delete;
+
 	void connect();
 	void switchDatabase(const std::string& name);
 
